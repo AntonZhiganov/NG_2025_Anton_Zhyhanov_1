c8efad116f3c7777ce45f5dc033675b4cce4d1d4
AntonZhyhanov <antohajigan@gmail.com>
2025-11-05 10:24:16 +0100

    Lecture 1, task 2 completed, gitignor added

---
+++ Lesson_1/Task_2/main/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int action = 0;
+    double number1;
+    double number2;
+    cout << "Calculator" << endl;
+    cout << "What do you want?" << endl;
+    cout << "1 - + " << endl;
+    cout << "2 - - " << endl;
+    cout << "3 - * " << endl;
+    cout << "4 - / " << endl;
+    cout << "5 - square" << endl;
+    cout << "6 - square root" << endl;
+    cin >> action;
+
+    if (action == 1|| action == 2|| action == 3|| action == 4) {
+        cout << "enter number 1: ";
+        cin >> number1;
+        cout << "enter number 2: ";
+        cin >> number2;
+    }
+
+    else if (action == 5 || action == 6){
+        cout << "enter number : ";
+        cin >> number1;
+    }
+
+    else {
+        cout << "Enter a number from 1 to 6";
+        return 0;
+    }
+
+    switch(action){
+        case 1:
+        cout << number1 + number2 << endl;
+            break;
+        case 2:
+        cout << number1 - number2 << endl;
+            break;
+        case 3:
+        cout << number1 * number2 << endl;
+            break;
+        case 4:
+        cout << number1 / number2 << endl;
+            break;
+        case 5:
+        cout << number1 * number1 << endl;
+            break;
+        case 6:
+        cout << sqrt(number1) << endl;
+            break;
+    };
+
+    return 0;
+}
